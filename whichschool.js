903fa1e83941af8b071b108f787b06bb45f52cd2
Jingu Kang <jinguk0816@gmail.com>
2021-03-15 01:51:18 +0000

    a program to let the clients to decide which school to attend between a variety of age.

---
+++ whichschool.js
@@ -0,0 +1,29 @@
+/*
+CHALLENGE!
+
+Complete the logic in the function whichSchool.
+
+The logic should help learners decide which school they should attend. Replace the comment inside the function with your code.
+
+The function returns one of the following strings, based on the value of age:
+
+1.Elementary School if age is below 13
+2.Secondary School if age is between 13 and 18 (both inclusive)
+3.Lighthouse Labs in all other cases.
+
+*/
+
+const whichSchool = function(age){
+  if(age < 13 && age >= 0){
+    console.log("Age below 13 should be attending Elementary School.");
+    return "Elementary School";
+  }else if(age >= 13 && age <= 18){
+    console.log("Age between 13 and 18 should attend Secondary School");
+    return "Secondary School";
+  }else{
+    console.log("Age above 19(including 19) should attend Lighthouse Labs!");
+    return "Lighthouse Labs";
+  }
+}
+
+whichSchool(15);
