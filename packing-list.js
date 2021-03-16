ec912a51cc88873d751f700f3d09775fa05b9fb7
Jingu Kang <jinguk0816@gmail.com>
2021-03-16 17:24:56 +0000

    also used while loop to list the items in an array.

--- packing-list.js
+++ packing-list.js
@@ -1,8 +1,19 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
-
+/*
+//Use for loop to show list of items in an array.
 console.log("Kitchen stuff to pack:");
 for(let i = 0; i < packingList.length; i++){
   console.log(packingList[i]);
 }
 
 packingList[3];
+
+*/
+
+//Use while loop to show list of items in an array.
+console.log("Kitchen stuff to pack:")
+let i = 0;
+while(i < packingList.length){
+  console.log(packingList[i]);
+  i++;
+}
